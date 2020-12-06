73a2d3950ab813759a5da59d47ae323c2df49633
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-06 12:00:05 +0600

    store added to admin

--- Agri_bazar/store/admin.py
+++ Agri_bazar/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class Admin_Product(admin.ModelAdmin):
+    list_display = ['name', 'price', 'weight', 'category']
+
+admin.site.register(Product)
--- Agri_bazar/store/models.py
+++ Agri_bazar/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.IntegerField(default=0)
+    weight = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='photos/product/%y/%m/%d')
