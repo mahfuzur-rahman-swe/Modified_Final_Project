59d3abd675456427c6476778a1b2db299a925ea1
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-12 12:53:25 +0600

    Product layout updated

--- Agri_bazar/pages/views.py
+++ Agri_bazar/pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'pages/index.html')
+    product_list = Product.objects.all()
+    context = {
+        'product_dict': product_list
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
