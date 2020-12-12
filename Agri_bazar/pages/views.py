d0cb61235d6ab5a2085f8aedd4df21f749865094
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-12 15:10:20 +0600

    category updated

--- Agri_bazar/pages/views.py
+++ Agri_bazar/pages/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 # Create your views here.
 
 def index(request):
     product_list = Product.objects.all()
+    category_list = Category.objects.all()
     context = {
-        'product_dict': product_list
+        'product_dict': product_list,
+        'category_dict': category_list
     }
-
     return render(request, 'pages/index.html', context)
 
 def about(request):
