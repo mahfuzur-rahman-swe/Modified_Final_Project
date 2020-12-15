a996e0081919600218e0e06a37e8b5e3db34a34c
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-15 23:25:19 +0600

    return value in signup form after error

--- Agri_bazar/pages/views.py
+++ Agri_bazar/pages/views.py
@@ -35,15 +35,48 @@ def signup(request):
         address = request.POST.get('Address')
         password = request.POST.get('Password')
 
-        data = Customer(first_name=first_name,
-                        last_name=last_name,
-                        mobile_number=mobile_num,
-                        email=email,
-                        address=address,
-                        password=password)
-        data.save()
+        #validation
+        error_message = None
 
-        return HttpResponse("success")
+        if not first_name:
+            error_message = 'First Name Required !!'
+        elif len(first_name) < 4:
+            error_message = 'First Name must be four character long'
+        elif not last_name:
+            error_message = 'Last Name Required !!'
+        elif len(first_name) < 4:
+            error_message = 'Last Name must be four character long'
+        elif not mobile_num:
+            error_message = 'Mobile Number Required !!'
+        elif len(mobile_num) < 11:
+            error_message = 'Mobile Number must be eleven character long'
+        elif not password:
+            error_message = 'Password Required !!'
+        elif len(password) < 5:
+            error_message = 'Passwords must be at least 5 characters long'
+        values = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'mobile_number': mobile_num,
+            'email': email,
+            'address': address,
+        }
+
+        if not error_message:
+            data = Customer(first_name=first_name,
+                            last_name=last_name,
+                            mobile_number=mobile_num,
+                            email=email,
+                            address=address,
+                            password=password)
+            data.save()
+            return render(request, 'pages/signup.html')
+        else:
+            dictionary = {
+                'error': error_message,
+                'value': values
+            }
+            return render(request, 'pages/signup.html', dictionary)
 
 def single_product(request):
     return render(request, 'pages/single_product.html')
